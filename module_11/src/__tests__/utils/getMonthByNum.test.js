9bce7fe44cf6659b11306cc3a27304a33d357aff
No-detka <no-detka@mail.ru>
2024-08-11 11:55:31 +0300

    Add files via upload

---
+++ module_11/src/__tests__/utils/getMonthByNum.test.js
@@ -0,0 +1,25 @@
+/*
+ *Тесты:
+ * 1. Введено не число
+ * 2. Введенное число меньше 1 или больше 12
+ * 3. Значение не введено (пусто)
+ * 4. Введено число от 1 до 12 и получен месяц
+ */
+import { getMonthByNum } from "../../utils/getMonthByNum.js";
+describe("test for getMonthByName function", () => {
+  it("input NaN", () => {
+    expect(getMonthByNum("string")).toBe("неизвестно");
+  }),
+    it("input number >1", () => {
+      expect(getMonthByNum(-12)).toBe("неизвестно");
+    }),
+    it("input number >1", () => {
+      expect(getMonthByNum(135)).toBe("неизвестно");
+    }),
+    it("input empty", () => {
+      expect(getMonthByNum()).toBe("неизвестно");
+    }),
+    it("input correct number", () => {
+      expect(getMonthByNum(2)).toBe("февраль");
+    });
+});
